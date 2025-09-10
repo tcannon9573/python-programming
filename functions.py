15498bb95801f723815ba04efb753cbae23911a0
tcannon9573 <tcannon9573@floridapoly.edu>
2025-09-10 17:12:57 +0000

    hello alice

--- functions.py
+++ functions.py
@@ -2,3 +2,9 @@
 ##s = "BI1BKDTPEPQAR"
 ##attend = s [::3]
 
+def greet(name):
+    message = f"Hello, {name}!"
+    return message
+#calling the function
+result = greet("Alice")
+print(result) #output: Hello Alice!
