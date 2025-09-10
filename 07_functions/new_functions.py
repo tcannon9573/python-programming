5a49f752a5fa52f525402d8b9b0c3ce08778b736
tcannon9573 <tcannon9573@floridapoly.edu>
2025-09-10 17:34:36 +0000

    check it out later

--- 07_functions/new_functions.py
+++ 07_functions/new_functions.py
@@ -1,7 +1,22 @@
 #Function that creates a message
 def create_message(prefix, *args):
-    s = " "
-    print(s.join(args))
+    s = str(prefix) + ": "
+    for arg in args:
+        s += str(arg) + " "
+    print(s)
+
+    def create_message(*args, **kwargs):
+        #Find the key in kwargs
+        separator = kwargs.get("separator", " ")
+        s = ""
+        for arg in args:
+            s += str(arg) + separator
+
+        return s[:-1]
+
 
 #call a function
 create_message("status", "Temp", 25, "Degree")
+create_message("Alert", "System", "is", "online")
+
+create_message("a", "b", separator = "-")
